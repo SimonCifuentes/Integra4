ed421b7fe749355ee379c213c920bd9e1f6288c0
Jecar <buzolargo50@gmail.com>
2025-09-02 22:00:34 -0400

    feat: agregar módulo features en mobile

---
+++ mobile/src/features/features/promociones/hooks.ts
@@ -0,0 +1,3 @@
+﻿/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+export const hooks = {}; // TODO: implementar hooks (useQuery/useMutation)
