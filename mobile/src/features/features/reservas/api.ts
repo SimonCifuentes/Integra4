5026ce2d2f370cf88ccfc37ed635f6dce13a7e2d
Jecar <buzolargo50@gmail.com>
2025-09-02 22:00:34 -0400

    feat: agregar módulo features en mobile

---
+++ mobile/src/features/features/reservas/api.ts
@@ -0,0 +1,9 @@
+﻿import { http } from "@/src/services/http";
+import { R } from "@/src/config/routes";
+import type { Reserva } from "@/src/types";
+
+export const ReservasAPI = {
+  mias: () => http.get<Reserva[]>(R.reservas.mias).then(r=>r.data),
+  create: (body: { id_cancha:number; fecha_reserva:string; hora_inicio:string; hora_fin:string }) =>
+    http.post<Reserva>(R.reservas.create, body).then(r=>r.data),
+};
---
+++ mobile/src/features/features/uploads/api.ts
@@ -0,0 +1,4 @@
+﻿/* eslint-disable @typescript-eslint/no-explicit-any */
+import { http } from "@/src/services/http";
+import { R } from "@/src/config/routes";
+export const api = {}; // TODO: implementar endpoints de este módulo
